f114bd7776e760c2b184d65c72f340a0a352e3dd
Sohrab <sohrabseera@hotmail.com>
2018-04-15 23:28:51 -0700

    Tip calculation works

--- app/src/main/java/edu/us/ischool/sseera/tipcalc/MainActivity.kt
+++ app/src/main/java/edu/us/ischool/sseera/tipcalc/MainActivity.kt
@@ -5,26 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val button = findViewById(R.id.tipButton) as Button
-        button.setOnClickListener{view ->
-            toast.show()
-        }
+
+        val amountRef = findViewById(R.id.amount) as EditText
+
+        //Set a handler for amountRef to enable the tip button when it has good input
+        // amountRef.
+        button.setOnClickListener({view ->
+            val amount:Double = (amountRef.getText().toString().toDouble())
+            val TipDue:Double = TipCalc(amount);
+            val toast = Toast.makeText(applicationContext, "$"+TipDue, Toast.LENGTH_SHORT);
+                toast.show()
+
+        })
 
     }
 
-    val TipDue:String = TipCalc().toString();
+    fun TipCalc(amount: Double = 0.0, tip:Int = 15): Double{
 
-    val toast = Toast.makeText(applicationContext, TipDue, Toast.LENGTH_SHORT);
-
-    fun TipCalc(amount: Double = 100.0, tip:Int = 15): Double{
-
-        return amount * (tip /100);
+        return amount * (tip /100.0);
 
     }
 
